3c69e0936dc89477009a65959a4e4ad6eb3a59f4
PutivskyAE <artem.putivsky@gmail.com>
2019-10-10 15:00:00 +0300

    Lesson 1: All tasks are done. At first it was difficult, but when I understood everything, there were no more problems.

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -9,6 +9,7 @@ import java.lang.Math.sqrt
  *
  * Лежит ли точка (x, y) внутри окружности с центром в (x0, y0) и радиусом r?
  */
+
 fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
         sqr(x - x0) + sqr(y - y0) <= sqr(r)
 
@@ -18,6 +19,7 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
+
 fun isNumberHappy(number: Int): Boolean {
     return if ((number / 1000) + (number / 100) % 10 == (number % 100) / 10 + (number % 10)) true else false
 }
@@ -29,6 +31,7 @@ fun isNumberHappy(number: Int): Boolean {
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
+
 fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
     return if ((x1 == x2 || y1 == y2 || x1 - y1 == x2 - y2 || x1 + y1 == x2 + y2)) true else false
 }
@@ -39,12 +42,13 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean {
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
+
 fun daysInMonth(month: Int, year: Int): Int {
-    return if (year % 400 != 0 && year % 100 == 0 && month == 2) 28
-    else if (year % 4 != 0 && month == 2) 28
-    else if (month == 2) 29
-    else if (month == 4 || month == 6 || month == 9 || month == 11) 30
-    else 31
+    return if (year % 400 != 0 && year % 100 == 0 && month == 2) 28 // определение числа дней для февраля невисокосного года ч.1
+    else if (year % 4 != 0 && month == 2) 28 // определение числа дней для февраля невисокосного года ч.2
+    else if (month == 2) 29 // определение числа дней для февраля високосного года
+    else if (month == 4 || month == 6 || month == 9 || month == 11) 30 // определение числа дней для апреля, июня, сентября и ноября
+    else 31 // определение числа дней для января, марта, мая, июля, августа, октября, декабря
 }
 
 /**
@@ -54,6 +58,7 @@ fun daysInMonth(month: Int, year: Int): Int {
  * окружности с центром в (x2, y2) и радиусом r2.
  * Вернуть true, если утверждение верно
  */
+
 fun circleInside(x1: Double, y1: Double, r1: Double,
                  x2: Double, y2: Double, r2: Double): Boolean {
     return if (sqrt(sqr(x2 - x1) + sqr(y2 - y1)) <= (r2 - r1)) true
@@ -70,12 +75,13 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
+
 fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
-    return if (a <= r && c <= s) true
-    else if (c <= r && a <= s) true
-    else if (c <= r && b <= s) true
-    else if (b <= r && c <= s) true
-    else if (b <= r && a <= s) true
-    else if (a <= r && b <= s) true
-    else false
+    return if (a <= r && c <= s) true // кирпич пройдет сквозь отверстие стены если строна а и сторона с меньше сторон r и s в соответственно
+    else if (c <= r && a <= s) true   // кирпич пройдет сквозь отверстие стены если строна с и сторона a меньше сторон r и s в соответственно
+    else if (c <= r && b <= s) true   // кирпич пройдет сквозь отверстие стены если строна с и сторона b меньше сторон r и s в соответственно
+    else if (b <= r && c <= s) true   // кирпич пройдет сквозь отверстие стены если строна b и сторона с меньше сторон r и s в соответственно
+    else if (b <= r && a <= s) true   // кирпич пройдет сквозь отверстие стены если строна b и сторона a меньше сторон r и s в соответственно
+    else if (a <= r && b <= s) true   // кирпич пройдет сквозь отверстие стены если строна а и сторона b меньше сторон r и s в соответственно
+    else false                        // кирпич не пройдет сквозь отверстие стены
 }
